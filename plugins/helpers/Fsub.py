c7920beea9ead373f4e830cf9869a692ef75e225
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2024-10-03 16:14:17 +0530

    Update Fsub.py

--- plugins/helpers/Fsub.py
+++ plugins/helpers/Fsub.py
@@ -1,49 +1,58 @@
 import os
-from pyrogram import Client, filters, enums
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-# Load environment variables
-load_dotenv()
+# Get the channel URL from the environment variable
+FSUB_URL = os.getenv("FSUB_URL", "https://t.me/your_channel")  # Set your default channel URL here
 
-# Environment variables
-FSUB_URL = os.getenv("FSUB_URL")  # Join channel link
-
-# Handle /start command
-@Client.on_message(filters.command("start") & filters.private)
-async def start_handler(client, message: Message):
+@Client.on_message(filters.command(["start"]))
+async def start(client, message):
     user_id = message.from_user.id
-
-    # Send the "Join Channel" and "Try Again" buttons if the user is not verified
-    btn = [
-        [InlineKeyboardButton("Join Channel", url=FSUB_URL)]
-    ]
     
-    # Check if the start command contains a file reference
-    if len(message.command) > 1 and message.command[1] != "subscribe":
-        try:
-            kk, file_id = message.command[1].split("_", 1)
-            pre = 'checksubp' if kk == 'filep' else 'checksub'
-            btn.append([InlineKeyboardButton("🔄 Try Again", callback_data=f"{pre}#{file_id}")])
-        except (IndexError, ValueError):
-            btn.append([InlineKeyboardButton("🔄 Try Again", url=f"https://t.me/{client.username}?start={message.command[1]}")])
+    # Check if the user is a member of the channel
+    try:
+        member = await client.get_chat_member(FSUB_URL, user_id)
+        if member.status in ["member", "administrator", "creator"]:
+            # If the user is already a member, proceed with sending the requested file
+            await message.reply("You are already a member! I'll send you the movie now.")
+            # Here you can send the requested file using its file_id
+            # Example: await message.reply_document(file_id='FILE_ID')
+        else:
+            raise Exception("User is not a member")
     
-    await client.send_message(
-        chat_id=message.from_user.id,
-        text="Please Join My Updates Channel to use this Bot!",
-        reply_markup=InlineKeyboardMarkup(btn),
-        parse_mode=enums.ParseMode.MARKDOWN
-    )
+    except:
+        # If the user is not a member, send the FSUB request with buttons
+        join_button = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("📢 Request to Join Channel 📢", url=FSUB_URL)],
+                [InlineKeyboardButton("🔄 Try Again 🔄", callback_data="try_again")]
+            ]
+        )
 
-# Handle Try Again button via CallbackQuery
-@Client.on_callback_query(filters.regex(r"^checksub#(.+)"))
-async def check_subscription_callback(client, callback_query: CallbackQuery):
+        fsub_message = (
+            "♦️ READ THIS INSTRUCTION ♦️\n\n"
+            "🗣 If you want to get the movie you requested, you must request to join our channel first by "
+            "clicking the '📢 Request to Join Channel 📢' button or the link below. After joining, click the "
+            "'🔄 Try Again 🔄' button to receive the movie.\n\n"
+            "👇 CLICK 'REQUEST TO JOIN CHANNEL' THEN CLICK 'TRY AGAIN' 👇"
+        )
+        
+        await message.reply(fsub_message, reply_markup=join_button)
+
+# Callback query handler for the "🔄 Try Again 🔄" button
+@Client.on_callback_query(filters.regex("try_again"))
+async def try_again(client, callback_query):
     user_id = callback_query.from_user.id
-    file_id = callback_query.data.split("#")[1]
-
-    # Since we aren't checking subscription, we assume the user has joined
-    # Delete the old message
-    await callback_query.message.delete()
-
-    # Send the file after they click Try Again
-    await client.send_document(chat_id=user_id, document=file_id)
+    
+    # Check if the user is a member of the channel
+    try:
+        member = await client.get_chat_member(FSUB_CHANNEL, user_id)
+        if member.status in ["member", "administrator", "creator"]:
+            await callback_query.message.edit("Thank you for joining! I'll send you the movie now.")
+            # Here you can send the requested file using its file_id
+            # Example: await callback_query.message.reply_document(file_id='FILE_ID')
+        else:
+            raise Exception("User is not a member")
+    
+    except:
+        await callback_query.answer("You haven't joined the channel yet. Please join and try again.", show_alert=True)
